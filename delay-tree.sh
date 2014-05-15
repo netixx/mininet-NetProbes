f209c65356cd33d2e2082db8b4772d6d3ece918d
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2014-05-15 16:00:41 +0200

    Addapted command line arguments to new builder

--- delay-tree.sh
+++ delay-tree.sh
@@ -5,5 +5,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 prog=$DIR"/start.sh"
 
-"$prog" --no-command --vars dtime=100000ms delay=300ms --start 30 --topo delay-tree --monitor usages/monitor.txt
-#"$prog" --command "$DIR/start-probe.sh {commandOpts}" --no-command --vars dtime=100000ms delay=500ms --start 30 --topo delay-tree
+#"$prog" --no-command --vars dtime=100000ms delay=300ms --start 30 --topo delay-tree --monitor usages/monitor.txt
+"$prog" --command "$DIR/start-probe.sh {commandOpts}" --vars dtime=1000000ms delay=20ms --auto-start-events 20 --topo delay-tree
