87aac4ffbc249e46b8324d933562513bc9ec7c44
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2014-05-23 16:19:31 +0200

    Generator takes subnet length as parameter

--- generators/flood-flat.sh
+++ generators/flood-flat.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+#usage flood-flat output_file nhosts subnetlength
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [[ "$3" -gt 8 ]]
+then
+echo "Subnet length must be <= 8"
+exit
+fi
+
 OUTPUT="$DIR/../mininet/data/$1.json"
 NHOSTS="$2"
-NHOSTPERPREFIXES="$3"
+PREFIX_LENGTH="$3"
+NHOSTPERPREFIXES="$(expr $(( 1 << $3 )) - 2)"
 
 hostname="h"
 linkname="l"
@@ -69,22 +77,26 @@ getprefix() {
     echo "$1.$2.$3"
 
 }
-vprefix="1"
+vprefix="0"
 prefixprefix="10.0"
-ip="$(getprefix "$prefixprefix" "$vprefix" "$(expr 1 % "$NHOSTPERPREFIXES")")"
-hosts="$(gethost "${hostname}1" "$ip")"
-links="$(getlink "$switchname" "${hostname}1")"
-
-for i in $(seq 2 $NHOSTS)
+#ip="$(getprefix "$prefixprefix" "$vprefix" "$(expr 1 % "$NHOSTPERPREFIXES")")"
+hosts=""
+#$(gethost "${hostname}1" "$ip")"
+links=""
+#$(getlink "$switchname" "${hostname}1")"
+sep=""
+for i in $(seq 0 $(expr $NHOSTS - 1))
 do
+    hnum="$(expr $i + 1)"
     lip=$(expr "$i" % "$NHOSTPERPREFIXES")
     if  [ "$lip" -eq 0 ]
     then
         vprefix=$(expr "$vprefix" + 1)
     fi
     ip="$(getprefix "$prefixprefix" "$vprefix" "$(expr "$lip" + 1)")"
-    hosts=$hosts",$(gethost "$hostname$i" "$ip")"
-    links=$links",$(getlink "$switchname" "$hostname$i")"
+    hosts="$hosts$sep$(gethost "$hostname$hnum" "$ip")"
+    links="$links$sep$(getlink "$switchname" "$hostname$hnum")"
+    sep=","
 done
 
 out=$(getjson "$(gethosts "$hosts")" "$(getswitch "$switchname")" "$(getlinks "$links")")
