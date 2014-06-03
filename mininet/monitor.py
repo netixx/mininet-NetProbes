1041272070ecdde9596a4dd64e775663d3c258be
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2014-06-03 16:53:26 +0200

    Added details for counters in monitoring

--- mininet/monitor.py
+++ mininet/monitor.py
@@ -18,13 +18,13 @@ rules = [{
 
 
 # _MON_COLLECT_TPL = "sudo iptables -n -L {direction} -v | awk '/pkts/ || /dpt:/ {{print $$1,$$2,$$NF}}' | sort -nk1"
-_MON_COLLECT_TPL = "sudo iptables -n -L {direction} -v"
+_MON_COLLECT_TPL = "sudo iptables -n -L {direction} -v -x"
 _MON_START_TPL = "sudo iptables -A {direction} -p {protocol} --dport {port}"
 _MON_STOP_TPL = "sudo iptables -D {direction} -p {protocol} --dport {port}"
 
 
 _directions = ['INPUT', 'OUTPUT']
-_CHAIN_TPL = r"^Chain (%s)" % r"|".join(_directions)
+_CHAIN_TPL = r"^Chain (?P<direction>%s) \(policy\s+(?P<policy>\w+)\s+(?P<packets>\d+)\s+packets,\s+(?P<bytes>\d+)\s+bytes\s*\)" % r"|".join(_directions)
 _PACKET_SPECS = r"|".join([r'{protocol} dpt:{port}'.format(**rule) for rule in rules])
 _PACKETS_TPLS = r'^\s+(\d+)\s+(\d+[KMG]?).+?({specs})'.format(specs = _PACKET_SPECS)
 
@@ -88,24 +88,30 @@ def _parseOutput(output, counter):
         if chain is not None:
             out += line+"\n"
             out += "{:<12} {:10} {:10}\n".format('proto:port', 'packets', 'bytes')
-            direction = chain.group(1)
-        m = re.match(_PACKETS_TPLS, line)
-        if m is not None:
+            direction = chain.group("direction")
             if counter is not None:
-                u = re.match(r'(\d+)(K|M|G)', m.group(2))
-                if u is not None:
-                    nbytes = int(u.group(1))
-                    if u.group(2) == 'K':
-                        nbytes *= 1024
-                    elif u.group(2) == 'M':
-                        nbytes *= 1024 ** 2
-                    elif u.group(2) == 'G':
-                        nbytes *= 1024 ** 3
-                else:
-                    nbytes = int(m.group(2))
-                counter.counters[direction]['bytes'] += nbytes
-                counter.counters[direction]['packets'] += int(m.group(1))
-            out += "{:<12} {:<10} {:<10}\n".format(m.group(3).replace(r' dpt', ''), m.group(1), m.group(2))
+                counter.counters[direction]['bytes'] += int(chain.group('bytes'))
+                counter.counters[direction]['packets'] += int(chain.group('packets'))
+        m = re.match(_PACKETS_TPLS, line)
+        if m is not None and direction is not None:
+            specs = m.group(3).replace(r' dpt', '')
+            if counter is not None:
+                # u = re.match(r"(\d+)(K|M|G)", m.group(2))
+                # if u is not None:
+                #     nbytes = int(u.group(1))
+                #     if u.group(2) == 'K':
+                #         nbytes *= 1024
+                #     elif u.group(2) == 'M':
+                #         nbytes *= 1024 ** 2
+                #     elif u.group(2) == 'G':
+                #         nbytes *= 1024 ** 3
+                # else:
+                nbytes = int(m.group(2))
+                if not counter.counters[direction].has_key(specs):
+                    counter.counters[direction][specs] = Counter.newCounter()
+                counter.counters[direction][specs]['bytes'] += nbytes
+                counter.counters[direction][specs]['packets'] += int(m.group(1))
+            out += "{:<12} {:<10} {:<10}\n".format(specs, m.group(1), m.group(2))
 
 
     return out + "\n"
@@ -116,11 +122,17 @@ class Counter(object):
     def __init__(self):
         self.counters = {}
         for d in _directions:
-            self.counters[d] = {'bytes' : 0,
-                                'packets' : 0}
+            self.counters[d] = self.newCounter()
+
+    @staticmethod
+    def newCounter():
+        return {'bytes': 0,
+                'packets': 0}
 
     def printAll(self):
         o = "Packets/Bytes summary :\n"
         for d in _directions:
-            o += "Direction {:<10}: packets : {packets:<10}, bytes : {bytes:<10}\n".format(d, **self.counters[d])
+            o += "Total for direction {:<10}: packets : {packets:<10}, bytes : {bytes:<10}\n".format(d, **self.counters[d])
+            for specs in [k for k in self.counters[d].iterkeys() if k not in self.newCounter().iterkeys()]:
+                o += "   Rule {:<15}: packets : {packets:<10}, bytes : {bytes:<10}\n".format(specs, **self.counters[d][specs])
         return o
