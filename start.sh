da4880f24203cd407981647aabb8d4dc8b749fca
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2014-06-03 16:52:45 +0200

    Added more ulimits

--- start.sh
+++ start.sh
@@ -10,6 +10,19 @@ prog=$DIR"/mininet/builder.py"
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
 ulimit -n 100000
+ulimit -s unlimited
+ulimit -c unlimited
+ulimit -e unlimited
+ulimit -i unlimited
+ulimit -l unlimited
+#ulimit -p 10
+ulimit -q unlimited
+ulimit -r unlimited
+ulimit -s unlimited
+ulimit -u unlimited
+
+ulimit -a
+
 "$py" "$prog" "$@"
 
 #sudo mn --clean
